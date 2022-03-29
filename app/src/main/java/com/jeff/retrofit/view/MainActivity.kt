0691e50198eb2f5ed08fc7f9d34241613c7fabb4
jeff <geoffreyerastus956@gmail.com>
2022-03-29 19:36:30 -0400

    initial

--- app/src/main/java/com/jeff/retrofit/view/MainActivity.kt
+++ app/src/main/java/com/jeff/retrofit/view/MainActivity.kt
@@ -1,16 +1,13 @@
 package com.jeff.retrofit.view
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.jeff.retrofit.databinding.ActivityMainBinding
 import com.jeff.retrofit.repository.Repository
 import com.jeff.retrofit.viewModel.MainViewModel
-import com.jeff.retrofit.databinding.ActivityMainBinding
 import com.jeff.retrofit.viewModel.MainViewModelFactory
-import java.lang.Exception
-import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     lateinit var viewModel: MainViewModel
@@ -33,17 +30,17 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
-        private fun getQuote(){
+
+    private fun getQuote() {
         try {
             viewModel.getQuote()
-            viewModel.response.observe(this,{
-                response ->
-                if (response.isSuccessful){
-                    Log.i("MainActivity","${response.body()?.quote}")
+            viewModel.response.observe(this) { response ->
+                if (response.isSuccessful) {
+                    Log.i("MainActivity", "${response.body()?.quote}")
                 }
-            })
-        }catch (e:Exception){
-            Log.e("MainActivity",e.toString())
+            }
+        } catch (e: Exception) {
+            Log.e("MainActivity", e.toString())
         }
 
     }
